131e1e6187fa06727016fee5741abaa70dc7b644
Jynnmei <jynn_mei@yahoo.com>
2025-04-01 01:15:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -45,8 +45,6 @@ window.addEventListener("load", function () {
 });
 
 // ============================================================================================
-// newscript
-
 window.addEventListener("load", function () {
   const canvas = document.querySelector(".game-canvas");
   const ctx = canvas.getContext("2d");
@@ -58,27 +56,48 @@ window.addEventListener("load", function () {
       this.game = game;
       window.addEventListener("keydown", (e) => {
         this.game.lastKey = "P" + e.key;
+        // console.log(this.game.lastKey);
       });
       window.addEventListener("keyup", (e) => {
         this.game.lastKey = "R" + e.key;
+        // console.log(this.game.lastKey);
       });
     }
   }
 
-  class npc1 {
+  // Player1
+  class Player1 {
     constructor(game) {
       this.game = game;
-      this.width = 32;
-      this.height = 32;
-      this.x = 150;
-      this.y = 150;
+      this.spriteWidth = 32;
+      this.spriteHeight = 32;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 3;
+      this.frameTimer = 0;
+      this.frameInterval = 10; // Adjust this to slow down animation
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.x = 95;
+      this.y = 180;
       this.speedX = 0;
       this.speedY = 0;
-      this.maxSpeed = 1;
+      this.maxSpeed = 2;
+      this.image = new Image();
+      this.image.src = "/character/player1.png";
     }
     draw(context) {
-      context.fillStyle = "red";
-      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.spriteWidth,
+        this.frameY * this.spriteHeight,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
     }
     setSpeed(speedX, speedY) {
       this.speedX = speedX;
@@ -88,42 +107,152 @@ window.addEventListener("load", function () {
     update() {
       if (this.game.lastKey == "PArrowLeft") {
         this.setSpeed(-this.maxSpeed, 0);
+        this.frameY = 3;
+      } else if (this.game.lastKey == "RArrowLeft") {
+        this.setSpeed(0, 0);
+        this.frameY = 3;
       } else if (this.game.lastKey == "PArrowRight") {
         this.setSpeed(this.maxSpeed, 0);
+        this.frameY = 1;
+      } else if (this.game.lastKey == "RArrowRight") {
+        this.setSpeed(0, 0);
+        this.frameY = 1;
       } else if (this.game.lastKey == "PArrowUp") {
         this.setSpeed(0, -this.maxSpeed);
+        this.frameY = 2;
+      } else if (this.game.lastKey == "RArrowUp") {
+        this.setSpeed(0, 0);
+        this.frameY = 2;
       } else if (this.game.lastKey == "PArrowDown") {
         this.setSpeed(0, this.maxSpeed);
+        this.frameY = 0;
+      } else if (this.game.lastKey == "RArrowDown") {
+        this.setSpeed(0, 0);
+        this.frameY = 0;
       } else {
         this.setSpeed(0, 0);
       }
       this.x += this.speedX;
       this.y += this.speedY;
 
-      const minX = this.game.leftMargin; 
-      const maxX = this.game.width - this.width - this.game.rightMargin; 
-      const minY = this.game.topMargin; 
-      const maxY = this.game.height - this.height; 
+      // horizontal & vertical walk
+      const minX = this.game.leftMargin; // 100 (left boundary)
+      const maxX = this.game.width - this.width - this.game.rightMargin; // Correct right boundary
+      const minY = this.game.topMargin; // 100 (top boundary)
+      const maxY = this.game.height - this.height; // Bottom boundary
 
       if (this.x < minX) this.x = minX;
       if (this.x > maxX) this.x = maxX;
       if (this.y < minY) this.y = minY;
       if (this.y > maxY) this.y = maxY;
+
+      // sprite animatoion
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
+      }
     }
   }
 
-  class npc3 {}
+  class computerPlayer {
+    constructor(game) {
+      this.game = game;
+      this.spriteWidth = 32;
+      this.spriteHeight = 32;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 3;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.x = 95;
+      this.y = 10;
+      this.speedX = 0;
+      this.speedY = 0;
+      this.maxSpeed = 2;
+      this.image2 = new Image();
+      this.image2.src = "/character/computerPlayer.png";
+    }
+    draw(context) {
+      context.drawImage(
+        this.image2,
+        this.frameX * this.spriteWidth,
+        this.frameY * this.spriteHeight,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+    setSpeed(speedX, speedY) {
+      this.speedX = speedX;
+      this.speedY = speedY;
+    }
+
+    update() {
+      if (this.game.lastKey == "PArrowLeft") {
+        this.setSpeed(-this.maxSpeed, 0);
+        this.frameY = 3;
+      } else if (this.game.lastKey == "RArrowLeft") {
+        this.setSpeed(0, 0);
+        this.frameY = 3;
+      } else if (this.game.lastKey == "PArrowRight") {
+        this.setSpeed(this.maxSpeed, 0);
+        this.frameY = 1;
+      } else if (this.game.lastKey == "RArrowRight") {
+        this.setSpeed(0, 0);
+        this.frameY = 1;
+      } else if (this.game.lastKey == "PArrowUp") {
+        this.setSpeed(0, -this.maxSpeed);
+        this.frameY = 2;
+      } else if (this.game.lastKey == "RArrowUp") {
+        this.setSpeed(0, 0);
+        this.frameY = 2;
+      } else if (this.game.lastKey == "PArrowDown") {
+        this.setSpeed(0, this.maxSpeed);
+        this.frameY = 0;
+      } else if (this.game.lastKey == "RArrowDown") {
+        this.setSpeed(0, 0);
+        this.frameY = 0;
+      } else {
+        this.setSpeed(0, 0);
+      }
+      this.x += this.speedX;
+      this.y += this.speedY;
+
+      // horizontal & vertical walk
+      const minX = this.game.leftMargin; // 100 (left boundary)
+      const maxX = this.game.width - this.width - this.game.rightMargin; // Correct right boundary
+      const minY = this.game.topMargin; // 100 (top boundary)
+      const maxY = this.game.height - this.height; // Bottom boundary
+
+      if (this.x < minX) this.x = minX;
+      if (this.x > maxX) this.x = maxX;
+      if (this.y < minY) this.y = minY;
+      if (this.y > maxY) this.y = maxY;
+
+      // sprite animatoion
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
+      }
+    }
+  }
 
   class Game {
     constructor(width, height) {
       this.width = width;
       this.height = height;
-      this.topMargin = 100;
-      this.leftMargin = 100;
-      this.rightMargin = 100;
+      this.topMargin = 85;
+      this.leftMargin = 92;
+      this.rightMargin = 92;
       this.lastKey = undefined;
       this.input = new InputHandler(this);
-      this.npc1 = new npc1(this);
+      this.player1 = new Player1(this);
+      this.computerPlayer = new computerPlayer(this);
       this.backgroundImg1 = new Image();
       this.backgroundImg1.onload = () => {
         this.render(ctx); 
@@ -132,26 +261,30 @@ window.addEventListener("load", function () {
 
       this.backgroundImg2 = new Image();
       this.backgroundImg2.onload = () => {
-        this.render(ctx); 
+        this.render(ctx);
       };
       this.backgroundImg2.src = "/image/snow-field.jpg";
     }
     render(context) {
       context.drawImage(this.backgroundImg1, 0, 0);
+
       context.drawImage(
         this.backgroundImg2,
-        10,
+        10, 
         200, 
         50, 
         50, 
         100, 
-        100,
-        150,
+        100, 
+        150, 
         150 
       );
 
-      this.npc1.update();
-      this.npc1.draw(context);
+      this.player1.update();
+      this.player1.draw(context);
+
+      this.computerPlayer.update();
+      this.computerPlayer.draw(context);
     }
   }
 
